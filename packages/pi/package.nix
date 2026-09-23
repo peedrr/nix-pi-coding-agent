cb5e4e1a4026ecba1d7997ae1cfc4221433589ca
peedrr <13527646+peedrr@users.noreply.github.com>
2026-09-23 12:34:20 +0000

    chore: update pi to v0.87.1

--- packages/pi/package.nix
+++ packages/pi/package.nix
@@ -4,7 +4,7 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "pi-coding-agent";
-  version = "0.87.0";
+  version = "0.87.1";
 
   # Synthetic npm root: package.json declares the published pi-coding-agent
   # tarball plus the optional packages that pi-subagents background children
@@ -12,7 +12,7 @@ buildNpmPackage (finalAttrs: {
   # materialises the whole tree, honouring the upstream npm-shrinkwrap.json.
   src = ./.;
 
-  npmDepsHash = "sha256-AKAX2mznIBZ1llk/sVFhMaF2QP4ZuxOZ5nDQ4HeV+7U=";
+  npmDepsHash = "sha256-T/XFRJxpGRU1jvxQ+nvF6e2YDdF+VxOdFbiFnxjTBYI=";
   npmDepsFetcherVersion = 2;
 
   npmRebuildFlags = [ "--ignore-scripts" ];
